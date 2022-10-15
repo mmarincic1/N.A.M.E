f40ccbe9a5159d400b0bcf90abf00ca6e413e322
nejlasubasic-e2x <nejla.subasic@e2x.com>
2022-10-15 18:31:34 +0200

    dio za korisnika gotov

--- NAME_frontend/src/components/korisnik/KorisnikHome.js
+++ NAME_frontend/src/components/korisnik/KorisnikHome.js
@@ -5,27 +5,27 @@ export default function KorisnikHome(props) {
 
     return (
         <>
-        <header class="masthead">
-            <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
-                <div class="d-flex justify-content-center">
-                    <div class="text-center">
-                        <h1 class="mx-auto my-0 text-uppercase">Dobro Došli</h1>
-                        <h2 class="text-white-50 mx-auto mt-2 mb-5">Poruka dodatnog opisa za klijente</h2>
-                        <a class="btn btn-primary" href="#upitPedijatru">Uputnica</a>
+        <header className="masthead">
+            <div className="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
+                <div className="d-flex justify-content-center">
+                    <div className="text-center">
+                        <h1 className="mx-auto my-0 text-uppercase">Dobro Došli</h1>
+                        <h2 className="text-white-50 mx-auto mt-2 mb-5">Poruka dodatnog opisa za klijente</h2>
+                        <a className="btn btn-primary" href="#upitPedijatru">Uputnica</a>
                     </div>
                 </div>
             </div>
         </header>
-        <section class="about-section text-center" id="upitPedijatru">
-        <h3>Uputite poruku vašem doktoru</h3>
+        <section className="about-section text-center" id="upitPedijatru">
+        <h3 className='text-white'>Uputite poruku vašem doktoru</h3>
         <div className="Auth-form-container">
             <form className="Auth-form" id="requestForm">
                 <label>Zahtjev za doktora: Ime i prezime ljekara</label>
                 <div className="form-group mt-3">
                     <textarea className='textareaForm' name="request" form="requestForm" placeholder='Opišite vaš problem...' cols={50} rows={50} ></textarea>
                 </div>
-                <div className="d-grid gap-2 mt-3">
-                    <button type="submit" className="btn btn-primary">
+                <div className="d-grid gap-2 mt-3 divSredina">
+                    <button type="submit" className="btn btn-primary sredina">
                         Pošalji
                     </button>
                 </div>
